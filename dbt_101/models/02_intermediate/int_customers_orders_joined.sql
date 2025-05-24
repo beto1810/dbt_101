899f783a24e7ccfbf9ad4e8ec73df23f1fa5e5a3
Dat_Nguyen <dat.nguyen.1810@gmail.com>
2025-05-24 22:38:57 +0700

    update the sql code of test

--- dbt_101/models/02_intermediate/int_customers_orders_joined.sql
+++ dbt_101/models/02_intermediate/int_customers_orders_joined.sql
@@ -1,20 +1,20 @@
 select
-    c.id as customer_id,
-    concat(c.first_name, ' ', c.last_name) as customer_name,
-    count(o.id) as number_of_orders,
-    sum(oi.quantity * p.price) as total_amount_spent_usd,
-    sum(oi.quantity * p.price) as total_spent,
-    min(o.date) as first_order_date,
-    max(o.date) as last_order_date
+    c.customer_id as customer_id,
+    concat(c.customer_first_name, ' ', c.customer_last_name) as customer_name,
+    count(o.order_id) as number_of_orders,
+    sum(oi.quantity * p.product_price) as total_amount_spent_usd,
+    sum(oi.quantity * p.product_price) as total_spent,
+    min(o.order_date) as first_order_date,
+    max(o.order_date) as last_order_date
 from {{ ref('stg_orders') }} as o
 left join {{ ref('stg_order_items') }} as oi
-    on o.id = oi.order_id
+    on o.order_id = oi.order_id
 left join {{ ref('stg_customers') }} as c
-    on o.customer_id = c.id
+    on o.customer_id = c.customer_id
 left join {{ ref('stg_products') }} as p
-    on oi.product_id = p.id
+    on oi.product_id = p.product_id
 where
     oi.order_item_id is not null
-    and o.status != 'cancelled'
+    and o.order_status != 'cancelled'
 group by
     1, 2
--- dbt_101/models/03_marts/fct_sales_by_customer_and_product_category.sql
+++ dbt_101/models/03_marts/fct_sales_by_customer_and_product_category.sql
@@ -1,22 +1,21 @@
-{% set product_categories = dbt_utils.default__get_column_values(table=ref('stg_products'), column='category') %}
+{% set product_categories = dbt_utils.default__get_column_values(table=ref('stg_products'), column='product_category') %}
 
-
-SELECT 
-    customers.id AS customer_id,
+select 
+    customers.customer_id,
     {% for category in product_categories %}
-        SUM(CASE WHEN products.category = '{{ category }}' THEN order_items.quantity ELSE 0 END) AS {{ category | replace(" ", "_") | lower }}_units_sold
+        sum(case when products.product_category = '{{ category }}' then order_items.quantity else 0 end) as {{ category | replace(" ", "_") | lower }}_units_sold
         {% if not loop.last %},{% endif %}
     {% endfor %},
 
     {% for category in product_categories %}
-        SUM(CASE WHEN products.category = '{{ category }}' THEN order_items.quantity * products.price ELSE 0 END) AS AMOUNT_SPENT_{{ category | replace(" ", "_") | lower }}
+        sum(case when products.product_category = '{{ category }}' then order_items.quantity * products.product_price else 0 end) as amount_spent_{{ category | replace(" ", "_") | lower }}
         {% if not loop.last %},{% endif %}
     {% endfor %}
-FROM {{ ref('stg_orders') }} AS sales
-LEFT JOIN {{ ref('stg_order_items') }} AS order_items
-    ON sales.id = order_items.order_id
-LEFT JOIN {{ ref('stg_products') }} AS products
-    ON order_items.product_id = products.id
-LEFT JOIN {{ ref('stg_customers') }} AS customers
-    ON sales.customer_id = customers.id
-GROUP BY customers.id
+from {{ ref('stg_orders') }} as orders
+left join {{ ref('stg_order_items') }} as order_items
+    on orders.order_id = order_items.order_id
+left join {{ ref('stg_products') }} as products
+    on order_items.product_id = products.product_id
+left join {{ ref('stg_customers') }} as customers
+    on orders.customer_id = customers.customer_id
+group by 1
